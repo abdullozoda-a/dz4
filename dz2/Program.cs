3de9ff09ceecead1cb9ea59752e273a222a1ef40
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-01 14:07:32 +0500

    create

---
+++ dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number: ");int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number: ");int num2 = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (num1 >= num2)
+            {
+                num1 -= num2;
+                count++; 
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
